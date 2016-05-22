c6b73fad91ce70d16063a9bb3f8e8dce92598f9a
Daniel J Janiak <danieljjaniak@Daniels-MacBook-Air.local>
2016-05-22 10:44:42 -0600

    Update code to use #selector instead of string literal, add a border to the text field

--- MoveViewGetOutTheWay/ViewController.swift
+++ MoveViewGetOutTheWay/ViewController.swift
@@ -81,8 +81,9 @@ class ViewController: UIViewController {
     
     
     func subscribeToKeyboardNotifications() {
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        //NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
     }
     
     func unsubscribeFromKeyboardNotifications() {
